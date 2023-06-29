941b8d69a0eeb6482206a5fb40d8c50f9006bfb9
Louis <mille.louis1@gmail.com>
2023-06-29 12:26:50 +0200

    Initial Commit

---
+++ GuessTheFlagg/GuessTheFlaggApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlaggApp.swift
+//  GuessTheFlagg
+//
+//  Created by Louis Mille on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlaggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
