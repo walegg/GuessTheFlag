64ee1ed3f382549f96835fbe71a240967807d95f
Louis Mille <mille.louis1@gmail.com>
2023-07-01 19:03:44 +0200

    added game system with restart option and which error the user made when the answer is wrong

--- GuessTheFlagg/ContentView.swift
+++ GuessTheFlagg/ContentView.swift
@@ -13,6 +13,10 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var currentScore = 0
+    @State private var currentQuestion = 1
+    @State private var totalQuestion = 9
+    @State private var resetGame = false
+    
     
     var body: some View {
         ZStack {
@@ -32,8 +36,10 @@ struct ContentView: View {
                             .font(.subheadline.weight(.heavy))
                         Text(countries[correctAnswer])
                             .font(.largeTitle.weight(.semibold))
+                        Text("Question \(currentQuestion)/8")
                     }
                     .foregroundColor(.white)
+                    
                     
                     ForEach(0..<3) { number in
                         Button {
@@ -41,7 +47,7 @@ struct ContentView: View {
                         } label: {
                             Image(countries[number])
                                 .renderingMode(.original)
-                                //.clipShape(Capsule())
+                            //.clipShape(Capsule())
                                 .shadow(radius: 5)
                         }
                     }
@@ -61,27 +67,42 @@ struct ContentView: View {
             
         }
         .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue", action: askQuestion)
-        } message: {
-            Text("Your score is \(currentScore)")
-        }
+                    Button("Continue", action: askQuestion)
+                } message: {
+                    Text("Your score is \(currentScore)")
+                }
+        .alert(scoreTitle, isPresented: $resetGame) {
+                    Button("Restart", action: askQuestion)
+                    }
     }
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
             currentScore += 1
+            currentQuestion += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong, that's the flag of \(countries[number])"
             currentScore -= 1
+            currentQuestion += 1
         }
         showingScore = true
     }
     
     func askQuestion () {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        if currentQuestion == 9 {
+            scoreTitle = "No more questions"
+            currentQuestion = 0
+            currentScore = 0
+            resetGame = true
+        } else {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+        }
+        
     }
+    
+
 }
 
 struct ContentView_Previews: PreviewProvider {
